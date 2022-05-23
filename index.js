c7f24f22da5241246593812ed92c1569cc01608f
aimunhidden485 <aimunnharr@gmail.com>
2022-05-23 08:01:21 +0600

    single tool data api created

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-
+var jwt = require('jsonwebtoken')
 
 app.use(cors())
 app.use(express.json())
@@ -27,6 +27,21 @@ async function run(){
             const query={}
             const tools= await toolsCollection.find(query).toArray()
             res.send(tools)
+        })
+
+
+        // app.post('/login', (req,res)=>{
+        //   const email=req.body.userEmail
+          
+        //   const token=jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET)
+        //   res.send(token)
+        // })
+
+        app.get('/purchase/:id', async(req,res)=>{
+          const id=req.params.id
+          const query={_id:ObjectId(id)}
+          const tool= await toolsCollection.findOne(query)
+          res.send(tool)
         })
     }
     finally{
