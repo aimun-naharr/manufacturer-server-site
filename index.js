9cbb467383e2680a71ce66de8f6bdd6eb162563a
aimunhidden485 <aimunnharr@gmail.com>
2022-05-23 17:13:20 +0600

    order api created

--- index.js
+++ index.js
@@ -18,35 +18,50 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ay62o.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
-        await client.connect();
-        const toolsCollection=client.db("manufacturers").collection('tools')
+async function run() {
+  try {
+    await client.connect();
+    const toolsCollection = client.db("manufacturers").collection('tools')
+    const reviewCollection = client.db("manufacturers").collection('review')
+    const orderCollection = client.db("manufacturers").collection('orders')
+    
+    app.get('/tools', async (req, res) => {
+      const query = {}
+      const tools = await toolsCollection.find(query).toArray()
+      res.send(tools)
+    })
 
-        app.get('/tools', async(req,res)=>{
-            const query={}
-            const tools= await toolsCollection.find(query).toArray()
-            res.send(tools)
-        })
+
+    // app.post('/login', async(req, res) => {
+    //   const email = req.body.userEmail
+    //   const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET)
+    //   res.send(token)
+    // })
+
+    app.get('/purchase/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const tool = await toolsCollection.findOne(query)
+      res.send(tool)
+    })
 
 
-        // app.post('/login', (req,res)=>{
-        //   const email=req.body.userEmail
-          
-        //   const token=jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET)
-        //   res.send(token)
-        // })
+    app.post('/review', async (req, res) => {
+      const review = req.body
+      
+      const result = await reviewCollection.insertOne(review)
+      res.send(result)
+    })
 
-        app.get('/purchase/:id', async(req,res)=>{
-          const id=req.params.id
-          const query={_id:ObjectId(id)}
-          const tool= await toolsCollection.findOne(query)
-          res.send(tool)
-        })
-    }
-    finally{
+    app.post('/order', async(req,res)=>{
+      const order=req.body
+      const result=await orderCollection.insertOne(order)
+      res.send(result)
+    })
+  }
+  finally {
 
-    }
+  }
 }
 run().catch(console.dir)
 
