f983e95e3bf99e93a01a33c7238d9d8e8a9ad36c
aimunhidden485 <aimunnharr@gmail.com>
2022-05-26 13:23:09 +0600

    error solved

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ const verifyJWT = (req, res, next) => {
   const token = authHeader.split(' ')[1]
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
-      res.status(403).send({ message: 'forbidden access' })
+      return res.status(403).send({ message: 'forbidden access' })
+      
     }
     req.decoded = decoded
     next()
